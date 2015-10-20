d116b60733084fc2144b2b383b7a87f541e2a419
Guilherme Coelho <guilhermeismco@gmail.com>
2015-10-19 22:51:18 -0400

    first commit

---
+++ week 6 homework/Assets/enemyMover2.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyMover2 : MonoBehaviour {
+
+	private Transform startPoint, endPoint;
+	
+	public float percentage;
+	public float speed = 5;
+	public float rayDistance = 3;
+	
+	private int direction;
+	
+	void Start () {
+		
+		startPoint = GameObject.Find ("StartPoint2").transform;
+		endPoint = GameObject.Find ("Player").transform;
+		direction = 1;
+		
+	}
+	
+	
+	void Update () {
+		
+		transform.LookAt(GameObject.Find ("Player").transform.position);
+		
+		transform.position = Vector3.Lerp (startPoint.position, endPoint.position, percentage);
+		
+		percentage += Time.deltaTime/speed * direction;
+
+		RaycastHit hitInfo;
+		if (Physics.Raycast (transform.position, transform.forward, out hitInfo, rayDistance)) {
+			Invoke("loadLevel",1);
+		}
+		
+		//	if ((percentage > 1) || (percentage < 0)) {
+		//		
+		//	}
+		
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.red;
+		Gizmos.DrawRay (transform.position, transform.forward * rayDistance);
+	}
+
+	void loadLevel(){
+		Application.LoadLevel("FPS");
+	}
+}
---
+++ week 6 homework/Assets/rayLaser.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rayLaser : MonoBehaviour {
+
+	public float rayDistance = 3;
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+
+		RaycastHit hitInfo;
+		if (Physics.Raycast (transform.position, transform.forward, out hitInfo, rayDistance) && hitInfo.transform.tag == "Goals") {
+		Debug.Log ("hit it babes");
+			Application.LoadLevel("Final");
+
+		}
+	
+	}
+	void OnDrawGizmos() {
+		Gizmos.color = Color.red;
+		Gizmos.DrawRay (transform.position, transform.forward * rayDistance);
+	}
+
+
+
+}
